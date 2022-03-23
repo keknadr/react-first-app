114eba95400bc2133ccf040c9b3dd52f6b7bb671
keknadr <pslups23@gmail.com>
2022-03-23 16:13:54 +0300

    Addeddobavlena funktsiya dobavleniya novykh sotrudnikov

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -24,7 +24,7 @@ class EmployeesListItem extends Component {
     }
 
     render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase, rise} = this.state;
 
         let classNames = 'list-group-item d-flex justify-content-between'
@@ -40,7 +40,8 @@ class EmployeesListItem extends Component {
                 <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
                 <div className='d-flex justify-content-center align-items-center'>
-                    <button type="button"
+                    <button
+                        type="button"
                         className="btn-cookie btn-sm "
                         onClick={this.onIncrease}>
                         <i className="fas fa-cookie"></i>
@@ -48,7 +49,8 @@ class EmployeesListItem extends Component {
 
                     <button
                         type="button"
-                        className="btn-trash btn-sm ">
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
                         <i className="fas fa-trash"></i>
                     </button>
                     <i className="fas fa-star"></i>
